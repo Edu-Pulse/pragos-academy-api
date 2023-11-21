cc25a36923a93d50c211d6cb76373227c154bd9a
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-22 06:19:26 +0700

    initial project
    initial

---
+++ src/main/java/org/binar/pragosacademyapi/PragosAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package org.binar.pragosacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragosAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragosAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/binar/pragosacademyapi/PragosAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binar.pragosacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragosAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
