84903ea2bfbe7652b3660b3f435dd9ffe48ee8f7
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-22 08:32:17 +0700

    Membuat Userrepository

--- src/main/java/org/binar/pragosacademyapi/repository/UserRepository.java
+++ src/main/java/org/binar/pragosacademyapi/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.binar.pragosacademyapi.repository;
 
 import org.binar.pragosacademyapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
