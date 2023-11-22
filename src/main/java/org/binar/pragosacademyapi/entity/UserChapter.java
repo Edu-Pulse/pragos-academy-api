b9aed6430dc1ae36cfaeb01fedf350080ad27a12
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-22 22:58:32 +0700

    Create UserChapter.java

---
+++ src/main/java/org/binar/pragosacademyapi/entity/UserChapter.java
@@ -0,0 +1,26 @@
+package org.binar.pragosacademyapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_chapters")
+public class UserChapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "chapter_id", referencedColumnName = "id")
+    private Chapter chapter;
+    private Boolean isDone;
+}
