b977c9dc0bde20c57674d49e8346559cb2585cf2
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-24 00:59:01 +0700

    Create /categories endpoint

---
+++ src/main/java/org/binar/pragosacademyapi/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package org.binar.pragosacademyapi.controller;
+
+import org.binar.pragosacademyapi.entity.Category;
+import org.binar.pragosacademyapi.entity.dto.CategoryDto;
+import org.binar.pragosacademyapi.entity.response.Response;
+import org.binar.pragosacademyapi.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping(
+            value = "/categories",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Response<List<CategoryDto>>> listCategory(){
+        return ResponseEntity.ok(categoryService.listCategory());
+    }
+}
