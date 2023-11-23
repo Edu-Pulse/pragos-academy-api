432ce9cab487428277dd96573bc3b248b6555aa8
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-23 09:36:31 +0700

    create login endpoint

--- src/main/java/org/binar/pragosacademyapi/controller/UserController.java
+++ src/main/java/org/binar/pragosacademyapi/controller/UserController.java
@@ -1,7 +1,56 @@
 package org.binar.pragosacademyapi.controller;
 
+import org.binar.pragosacademyapi.config.TokenProvider;
+import org.binar.pragosacademyapi.entity.request.LoginRequest;
+import org.binar.pragosacademyapi.entity.response.Response;
+import org.binar.pragosacademyapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider tokenProvider;
+    private final UserService userService;
+    @Autowired
+    public UserController(AuthenticationManager authenticationManager, TokenProvider tokenProvider, UserService userService){
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+        this.userService = userService;
+    }
+
+    @GetMapping(
+            value = "/login",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Response<Map<String, String>>> login(@RequestBody LoginRequest request){
+        Response<Map<String, String>> response = new Response<>();
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = tokenProvider.generateToken(authentication);
+        response.setError(false);
+        response.setMessage("Login Berhasil");
+        Map<String, String> dataToken = new HashMap<>();
+        dataToken.put("token", token);
+        response.setData(dataToken);
+        return ResponseEntity.ok(response);
+    }
+
 }
