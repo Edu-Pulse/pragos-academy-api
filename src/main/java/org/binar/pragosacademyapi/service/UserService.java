4ae2f3e59d1c9c087d476e0b150a8d31d20a92f3
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-26 13:05:26 +0700

    edit parameter

--- src/main/java/org/binar/pragosacademyapi/service/UserService.java
+++ src/main/java/org/binar/pragosacademyapi/service/UserService.java
@@ -2,13 +2,17 @@ package org.binar.pragosacademyapi.service;
 
 import org.binar.pragosacademyapi.entity.dto.UserDto;
 import org.binar.pragosacademyapi.entity.request.RegisterRequest;
+import org.binar.pragosacademyapi.entity.request.UpdateUserRequest;
 import org.binar.pragosacademyapi.entity.response.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.Multipart;
 
 public interface UserService {
 
     Response<UserDto> getProfile();
     Response<String> register(RegisterRequest user);
-    Response<String> update(RegisterRequest updateUser);
+    Response<String> update(UpdateUserRequest updateUser);
     Response<String> update(String password);
     Response<String> generateCodeVerification(String email);
     Response<String> verification(String email, Integer code);
