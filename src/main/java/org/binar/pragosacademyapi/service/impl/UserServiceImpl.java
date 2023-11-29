c64a43c3f59e26f241237ea73239a2fc0b001910
Gunawan <92556282+Gunawann20@users.noreply.github.com>
2023-11-29 18:06:24 +0700

    Update UserServiceImpl.java

--- src/main/java/org/binar/pragosacademyapi/service/impl/UserServiceImpl.java
+++ src/main/java/org/binar/pragosacademyapi/service/impl/UserServiceImpl.java
@@ -179,6 +179,7 @@ public class UserServiceImpl implements UserService {
                 }
             }
         }catch (Exception e){
+            log.error(e.getCause().getMessage());
             response.setError(true);
             response.setMessage("Failed to update data "+e.getMessage());
             response.setData("Terjadi kesalahan");
