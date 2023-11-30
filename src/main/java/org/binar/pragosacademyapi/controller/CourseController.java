960db82152f582ca35503a56df4d14a0c6b170ef
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-11-30 16:23:18 +0700

    Add cross origin

--- src/main/java/org/binar/pragosacademyapi/controller/CourseController.java
+++ src/main/java/org/binar/pragosacademyapi/controller/CourseController.java
@@ -8,15 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class CourseController {
     @Autowired
     private CourseService courseService;
