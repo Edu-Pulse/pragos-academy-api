f31ebb825af8413b86bdb202eb88f5dee5d9a2e2
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-12-06 03:18:36 +0700

    update Mapping

--- src/main/java/org/binar/pragosacademyapi/config/WebConfig.java
+++ src/main/java/org/binar/pragosacademyapi/config/WebConfig.java
@@ -1,5 +1,6 @@
 package org.binar.pragosacademyapi.config;
 
+import io.netty.handler.codec.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,6 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                )
+                .allowedOrigins("*");
     }
 }
