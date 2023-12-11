e3c9dd81bc6a99463959cc8b48f0e25b9c273edd
Gunawan <92556282+Gunawann20@users.noreply.github.com>
2023-12-11 16:34:10 +0700

    Update UserServiceImpl.java

--- src/main/java/org/binar/pragosacademyapi/service/impl/UserServiceImpl.java
+++ src/main/java/org/binar/pragosacademyapi/service/impl/UserServiceImpl.java
@@ -125,7 +125,7 @@ public class UserServiceImpl implements UserService {
             newuser.setUserVerification(userVerification);
 
             userRepository.save(newuser);
-            response.setError(true);
+            response.setError(false);
             response.setMessage("Success");
 
             sendEmail(user.getEmail(), code);
