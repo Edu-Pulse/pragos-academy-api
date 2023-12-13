b3170d50c1317d92d643bae2e36e4179e9024475
Gunawan <gunawanatmoko75@students.unnes.ac.id>
2023-12-13 22:31:14 +0700

    Fix: payment user

--- src/main/java/org/binar/pragosacademyapi/entity/dto/PaymentUserDto.java
+++ src/main/java/org/binar/pragosacademyapi/entity/dto/PaymentUserDto.java
@@ -2,13 +2,18 @@ package org.binar.pragosacademyapi.entity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.binar.pragosacademyapi.enumeration.Level;
+
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class PaymentUserDto {
+    private String image;
     private String categoryName;
     private String courseName;
+    private String lecturer;
+    private Level level;
     private Boolean status;
     private String paymentMethod;
     private Long amount;
