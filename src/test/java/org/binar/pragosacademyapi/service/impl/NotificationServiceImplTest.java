daac73cc1ac834605553846d60ff0cd218148698
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-01-02 21:16:50 +0800

    unit test notification

--- src/test/java/org/binar/pragosacademyapi/service/impl/NotificationServiceImplTest.java
+++ src/test/java/org/binar/pragosacademyapi/service/impl/NotificationServiceImplTest.java
@@ -11,13 +11,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -25,34 +25,23 @@ public class NotificationServiceImplTest {
     @Mock
     private ExecutorService executorService;
 
-    @Mock
-    private FirebaseDatabase firebaseDatabase;
-
-    @Mock
-    private DatabaseReference databaseReference;
-
     @InjectMocks
     private NotificationServiceImpl notificationService;
 
     @Test
-    void testSendNotification_singleUser() {
+    void testSendNotification() {
         String email = "test@example.com";
-        String message = "This is a test notification";
+        String message = "Hello, this is a notification message.";
         notificationService.sendNotification(email, message);
         verify(executorService).submit(any(Runnable.class));
-        verify(firebaseDatabase).getReference(anyString());
-        verify(databaseReference).push();
-        verify(databaseReference).setValueAsync(any(Notification.class));
     }
 
     @Test
-    void testSendNotification_multipleUsers() {
-        List<String> usersEmail = List.of("user1@example.com", "user2@example.com");
-        String message = "This is a message for multiple users";
-        notificationService.sendNotification(usersEmail, message);
+    void testSendNotificationToList() {
+        String message = "Hello, this is a notification message.";
+        String email1 = "user1@example.com";
+        String email2 = "user2@example.com";
+        notificationService.sendNotification(Arrays.asList(email1, email2), message);
         verify(executorService).submit(any(Runnable.class));
-        verify(firebaseDatabase, times(2)).getReference(anyString());
-        verify(databaseReference, times(2)).push();
-        verify(databaseReference, times(2)).setValueAsync(any(Notification.class));
     }
 }
